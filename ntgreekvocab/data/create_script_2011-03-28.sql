97edc368aaeb9d7131027a4da25528f58435a6dd
Hain-Lee <leehsueh7@gmail.com>
2011-03-28 20:59:52 -0700

    data for lesson 33 vocab

---
+++ ntgreekvocab/data/create_script_2011-03-28.sql
@@ -0,0 +1,45 @@
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('transitive: I cause to rise. Intransitive: I stand up, I arise','ἀνίστημι','a -μι verb. Transitive meaning when in present, future, or 1st aorist. Intransitive meaning when in 2nd aorist, perfect active, or middle.','v','33','present, active, indicative, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('I seem, I think','δοκέω','','v','33','present, active, indicative, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('I am able','δύναμαι','deponent','v','33','present, active, indicative, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('power, a miracle','δύναμις, δυνάμεως','','n','33','feminine, singular, nominative','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('I went','ἔβην','2nd aorist of βαίνω','v','33','2nd aorist, active, indicative, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('another','ἕτερος, -α, -ον','sometimes, but not always, implies difference of kind, whereas ἄλλος often denotes mere numerical distinction','adj','33','masculine, feminine, neuter, singular, nominative','heterogeneous');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('transitive: I cause to stand. Intransitive: I stand.','ἵστημι','a -μι verb. Transitive meaning when in present, future, or 1st aorist. Intransitive meaning when in 2nd aorist, perfect active, or middle.','v','33','present, active, indicative, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('I sit','κάθημαι','deponent','v','33','present, middle, indicative, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('sitting','καθήμενος','','prtcpl','33','present, middle, indicative, singular, masculine, nominative','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('whole, all','ὅλος, -η, -ον','','adj','33','masculine, feminine, neuter, singular, nominative','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('like, similar','ὅμοιος, -α, -ον','takes the dative of that to which something is similar. opposite of ἕτερος.','adj','33','masculine, feminine, neuter, singular, nominative','homogeneous');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('I become near, I arrive, I come','παραγίνομαι','','v','33','present, middle, indicative, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('I make known, I reveal','φανερόω','','v','33','present, active, indicative, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('I say','φημί','μι verb with stem φα-; much less common than λέγω','v','33','present, active, indicative, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('as, when','ὡς','','adv','33','','');
