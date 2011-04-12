4f8b437b15eb5749c9278e58527438b5e8f4b13d
Hain-Lee <leehsueh7@gmail.com>
2011-04-11 22:09:18 -0700

    Added lesson 34 words, minor css animations for fun

---
+++ ntgreekvocab/data/create_script_2011-04-11.sql
@@ -0,0 +1,75 @@
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('then, therefore','ἄρα','','conj','34','','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('I destroy; middle, I perish','ἀπόλλυμι','-μι form of ἀπολλύω.','v','34','present, active, indicative, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('I destroy; middle, I perish','ἀπολλύω','','v','34','present, active, indicative, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('with gen., up to, as far as, until','ἄχρι','An improper preposition when followed by a genitive.','conj','34','','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('I show, I demonstrate','δείκνυμι','-μι form of δεικνύω.','v','34','present, active, indicative, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('I show, I demonstrate','δεικνύω','','v','34','present, active, indicative, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('therefore, for this reason','διό','','conj','34','','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('a strong desire, lust','ἐπιθυμία','','n','34','feminine, singular, nominative','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('I work','ἐργάζομαι','deponent','v','34','present, middle, indicative, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('until, while; as a preposition with gen., up to, as far as, unto','ἕως','An improper preposition when followed by a genitive. More common than ἄχρι and μέχρι.','conj','34','','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('tribulation','θλῖψις, -εως','','n','34','feminine, singular, nominative, genitive','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('a throne','θρόνος','','n','34','masculine, singular, nominative','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('middle, in the midst','μέσος, -η, -ον','','adj','34','masculine, feminine, neuter, singular, nominative','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('with gen., until, as far as, to the degree that; as a conj., until','μέχρι','An improper preposition when followed by a genitive.','conj','34','','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('I know','οἶδα','perfect tense with a present meaning; very common word in the Bible','v','34','perfect, active, indicative, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('I know (subjunctive)','εἱδῶ','','v','34','perfect, active, subjunctive, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('and, furthermore, also','τε','enclitic conjunction. τε is somewhat closer to and, while δέ is closer to but.','conj','34','','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('not only...but also','τε...τε','','construct','34','','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('I finish, I complete, I bring to an end','τελέω','','v','34','present, active, indicative, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('I will finish, I will complete, I will bring to an end','τελέσω','','v','34','future, active, indicative, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('I finished, I completed, I brought to an end','ἐτέλεσα','','v','34','1st aorist, active, indicative, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('a type, a pattern','τύπος','','n','34','masculine, singular, nominative','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('I shine; mid., I appear, I become visible','φαίνω','','v','34','present, active, indicative, 1st person, singular','');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('I flee','φεύγω','','v','34','present, active, indicative, 1st person, singular','fugitive');
+INSERT INTO ntgreekvocab_simplecard
+	(definition,greek_word,notes,part_of_speech,lesson_number,parsing_info,hints)
+	VALUES ('a need','χρεία','','n','34','feminine, singular, nominative','');
